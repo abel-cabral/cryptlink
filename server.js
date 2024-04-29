ec78b5cb259a457b4498dd8945fd5fea2f41cdf8
C1333760 Abel Cabral De Arruda <c1333760@interno.bb.com.br>
2024-04-29 04:52:02 -0300

    (fix): config server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const compression = require('compression');
 const path = require('path');
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 const server = express();
 
@@ -9,9 +9,9 @@ const DIST_DIR = path.join(__dirname, './web-build');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 server.use(express.static(DIST_DIR));
-server.use(compression);
+server.use(compression());
 
-server.get('/', (req, res) => {
+server.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
